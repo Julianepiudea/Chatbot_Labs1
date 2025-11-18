d0c8222176148104a24d6ae1c556e295bdc301d8
Julianepiudea <investigaciones@labmedico.com>
2025-11-18 12:07:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from langchain_community.vectorstores import FAISS
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain.chains import RetrievalQA
+from langchain.chains.retrieval_qa.base import RetrievalQA
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 import os
